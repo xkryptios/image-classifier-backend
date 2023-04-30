dd1aa37600c40b784626be344d8fd63d318aa650
xiang <linruixiang123@gmail.com>
2023-05-01 00:00:44 +0800

    Add inplementation for classification

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import tensorflow as tf
+from transformers import AutoImageProcessor ,TFAutoModelForImageClassification
+from PIL import Image
+# import requests
+# from tensorflow import keras
+# from keras.preprocessing.image import ImageDataGenerator
+from werkzeug.utils import secure_filename
 from flask import Flask, request
 import base64
 import uuid
@@ -5,30 +12,31 @@ from waitress import serve
 
 app = Flask(__name__)
 
+img = Image.open('./images/test.jpeg')
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+inputs = image_processor(img, return_tensors="tf")
+
+model = TFAutoModelForImageClassification.from_pretrained("google/vit-base-patch16-224")
 
 @app.route("/")
 def hello_world():
     print('this is dumb')
-    return "Hello, World!!"
+    return "Hello, World!!!"
 
 
-@app.route("/hi", methods=['POST'])
-def hello():
-    data = request.get_json()
-    print('test1')
-    print(data)
-    return "<p>Hello, World!@@!</p>"
-
-
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/classify', methods=['POST'])
+def classify():
     filename = uuid.uuid4()
-    photo_encoded = request.get_json()['photo']
-    photo = base64.b64decode(photo_encoded)
+    file = request.files['image']
+    file.save(f'images/{filename}.jpg')
+    print('File uploaded successfully')
+    print('Initialising classification...')
 
-    with open(f'images/{filename}.jpg','wb') as f:
-        f.write(photo)
-    # image = 
+    logits = model(**inputs).logits
+    predicted_class_id = int(tf.math.argmax(logits, axis=-1)[0])
+    result = model.config.id2label[predicted_class_id]
+    print('Classification Result:',result)
+    return result
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=8080)
